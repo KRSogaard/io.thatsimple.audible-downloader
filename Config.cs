a4584f5b69a44b4104988afd0202285ae1a4acc9
Kasper Sogaard <sogaardk@amazon.com>
2022-11-21 16:34:17 -0800

    Creating repo

---
+++ Config.cs
@@ -0,0 +1,35 @@
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace AudibleDownloader
+{
+    public static class Config
+    {
+        private static IConfiguration configuration;
+
+        static Config()
+        {
+            var builder =
+                new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appSettings.json",
+                    optional: true,
+                    reloadOnChange: true)
+                    .AddJsonFile("appSettings.Development.json",
+                    optional: true,
+                    reloadOnChange: true);
+            configuration = builder.Build();
+        }
+
+        public static string Get(string name)
+        {
+            string appSettings = configuration[name];
+            return appSettings;
+        }
+
+        public static IConfigurationSection GetSection(string name)
+        {
+            return configuration.GetSection(name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using RabbitMQ.Client;
+
+namespace AudibleDownloader {
+    class Listener {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello, World! \"" + Config.Get("KeyOne") + "\"");
+        }
+    }
+}
+
